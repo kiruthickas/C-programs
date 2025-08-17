c5d01a868c4fc30b349d30a98322eab82f186470
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-17 21:28:21 +0530

    Add files via upload

---
+++ arithmetic operators program.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b,add,sub,mul,div,mod;
+    printf("enter no");
+    scanf("%d%d",&a,&b);
+    add=a+b;
+    printf("add = %d",add);
+    sub=a-b;
+    printf(" sub=%d",sub);
+   mul=a*b;
+    printf(" sub=%d",sub);
+    div=a/b;
+   printf(" div=%d",div);
+    mod=a%b;
+    printf(" mod=%d",mod);
+}

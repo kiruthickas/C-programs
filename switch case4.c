2428becec88ecbe7cae8f6813a638e8d7b5a812c
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-23 22:50:16 +0530

    Add files via upload

---
+++ switch case4.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ int i = 3;
+ switch ( i )
+ {
+ case 0 :
+ printf ( "\nCustomers are dicey" ) ;
+ case 1 :
+ printf ( "\nMarkets are pricey" ) ;
+ case 2 :
+ printf ( "\nInvestors are moody" ) ;
+ case 3 :
+ printf ( "\nAt least employees are good" ) ;
+ }
+} 
+
+

fb1a6d609319dc812defc7e58597edff66eceb44
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-23 20:47:54 +0530

    Add files via upload

---
+++ switch case1.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+{
+ char suite = 1 ;
+ switch ( suite )
+ {
+ case 1 :
+ printf ( "\nDiamond" ) ;
+ case 2 :
+ printf ( "\nSpade" ) ;
+ default :
+ printf ( "\nHeart") ;
+ }
+ printf ( "\nI thought one wears a suite" ) ;
+}
+return 0;
+}

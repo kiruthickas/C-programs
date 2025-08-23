eead6ea9708b460d1e7f2129947c0fedb98ff30b
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-23 22:39:52 +0530

    Add files via upload

---
+++ switch case2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int c = 12 ;
+ switch ( c )
+ {
+ case 'v' :
+ printf ( "I am in case v \n" ) ;
+ break ;
+ case 3 :
+ printf ( "I am in case 3 \n" ) ;
+ break ;
+ case 12 :
+ printf ( "I am in case 12 \n" ) ;
+ break ;
+ default :
+ printf ( "I am in default \n" ) ;
+}
+}

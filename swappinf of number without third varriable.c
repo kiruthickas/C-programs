f9ab510112c8daa375b09559ad138d13e1d28ae0
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-24 17:35:27 +0530

    Add files via upload

---
+++ swappinf of number without third varriable.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter a and b");
+    scanf("%d %d",&a,&b);
+    printf("swpped value of a%d\n ",b);
+    printf("swpped value of b%d",a);
+    return 0;
+}

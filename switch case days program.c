c867bbfea055f531da37925483e9ff65b76fab19
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-29 00:50:52 +0530

    Add files via upload

---
+++ switch case days program.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int day;
+    printf("enter days");
+    scanf("%d",&day);
+    switch(day)
+    {
+    case 1:
+    printf("monday");
+    break;
+    case 2:
+    printf("tuesday");
+    break;
+    case 3:
+    printf("thursday");
+    break;
+     case 4:
+    printf("friday");
+    break;
+     case 5:
+    printf("saturday");
+    break;
+    case 6:
+    printf("sunday");
+    }
+}
+    
+    
+    

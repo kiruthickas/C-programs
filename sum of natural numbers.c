5f06f5c02d504edeefbcbd0e15576da7bb3c8c57
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-28 21:57:31 +0530

    Add files via upload

---
+++ sum of natural numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n,sum;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    sum=n*(n+1)/2;
+    printf("the sum of natural numbers %d",sum);
+}
+

2c96cae82a70896f1f29f736bb1edcbefac56129
kiruthickas <kiruthickasofficial@gmail.com>
2025-08-29 23:54:46 +0530

    Add files via upload

---
+++ great,small,same.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int num1,num2;
+    printf("enter a number");
+    scanf("%d%d",&num1,&num2);
+    if(num1==num2)
+    {
+        printf("it is same");
+    }
+        else if(num1>num2)
+        {
+            printf("num1 is greater");
+            
+        }
+        else if(num1<num2)
+        {
+            printf("num 1 is smaller");
+        }
+    }

19e9ae91bfd8453bc9c4c6297f1b4cff9e8812a5
kiruthickas <kiruthickasofficial@gmail.com>
2025-09-25 00:31:30 +0530

    Add files via upload

---
+++ sumofdigits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int n,i,sum=0;
+  printf("enter n");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+      i=n%10;
+      sum=sum+i;
+      n=n/10;
+      
+  }
+   printf("sum of digits %d\n",sum);
+}
